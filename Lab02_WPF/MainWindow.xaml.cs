6b28a3cb6597fe9e56decbb270750da9ceb583d8
gadem <gadem@GABRIEL>
2018-03-15 14:04:53 -0400

    first

---
+++ Lab02_WPF/MainWindow.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lab02_WPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            cboSemester.ItemsSource = new List<string> { "1", "2", "3", "4", "5", "6", "7", "8" };
+
+            lstCourses.ItemsSource = new List<string> { "COMP100", "COMP213", "COMP120", "COMP125", "COMP123", "COMP122", "MATH185", "COMM171" };
+
+        }
+
+        
+
+        
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            cboSemester.SelectedIndex = 1;
+            txtName.Text = "Gabriel De Marchi";
+
+            lstCourses.UnselectAll();
+
+            rbtnDomestic.IsChecked = true;
+              
+        }
+
+        private void btnOk_Click(object sender, RoutedEventArgs e)
+        {
+            string result;
+
+            result = "Name: " + txtName.Text + "Semester: " ;
+
+           result = cboSemester.Text + "\n Nationality: " ;
+
+           result = (rbtnDomestic.IsChecked ?? true) ? " DOMESTIC ":" INTERNATIONAL " + " Courses: ";
+
+            foreach (string item in lstCourses.SelectedItems)
+            {
+                 result += item + ", " ;
+            }
+
+            MessageBox.Show(result, "User Data", MessageBoxButton.OK);
+
+        }
+    }
+}
---
+++ Week00_Delegate/Program.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week00_Delegate
+{
+
+
+    using System;
+    using System.ComponentModel;
+    namespace EventSample
+    {
+        using System;
+        using System.ComponentModel;
+    
+   
+        public class AlarmEventArgs : EventArgs
+        {
+            private readonly bool snoozePressed;
+            private readonly int nrings;
+            private string v;
+
+            public AlarmEventArgs(string v)
+            {
+                this.v = v;
+            }
+
+       
+            public AlarmEventArgs(bool snoozePressed, int nrings)
+            {
+                this.snoozePressed = snoozePressed;
+                this.nrings = nrings;
+            }
+
+     
+            public int NumRings
+            {
+                get { return nrings; }
+            }
+
+        
+            public bool SnoozePressed
+            {
+                get { return snoozePressed; }
+            }
+
+        
+            public string AlarmText
+            {
+                get
+                {
+                    if (snoozePressed)
+                    {
+                        return ("Wake Up!!! Snooze time is over.");
+                    }
+                    else
+                    {
+                        return ("Wake Up!");
+                    }
+                }
+            }
+
+                public string Text { get; internal set; }
+            }
+
+        // Delegate declaration.
+        //
+        public delegate void AlarmEventHandler(object sender, AlarmEventArgs e);
+
+
+        public class AlarmEventProducer
+        {
+            private bool snoozePressed = false;
+            //private int nrings = 0;
+            private bool stop = false;
+
+          
+            public bool Stop
+            {
+                get { return stop; }
+                set { stop = value; }
+            }
+
+    
+            public event AlarmEventHandler Alarm;
+
+            protected virtual void OnAlarm(AlarmEventArgs e)
+            {
+                    //the following it not needed, it just focuses on the name of the delegate
+                    AlarmEventHandler handler = Alarm;
+
+                    //only invoke the handler if it is initialize
+                    if (handler != null)
+                        handler(this, e);
+
+                }
+
+        
+                public void RunAlarm()
+            {
+                    int count = 1;
+                    while (count <= 12)
+                    {
+                        System.Threading.Thread.Sleep(1000);
+                        //this raises the event
+                        OnAlarm(new AlarmEventArgs(count++.ToString() + " o'clock"));
+                    }
+
+                }
+            }
+
+        // The WakeMeUp class that has a method AlarmRang that handles the
+        // alarm event.
+        //
+        public class AlarmEventConsumer
+        {
+                private string name;
+
+                public  AlarmEventConsumer(string name)
+                {
+                    this.name = name;
+                }
+
+
+                public void AlarmRang(object sender, AlarmEventArgs e)
+                {
+                    Console.WriteLine(name + " was called with 1 o'clock " + e.Text);
+                }
+        }
+
+        // The driver class that hooks up the event handling method of
+        // WakeMeUp to the alarm event of an Alarm object using a delegate.
+        // In a forms-based application, the driver class is the
+        // form.
+        //
+        public class AlarmDriver
+        {
+            public static void Main(string[] args)
+            {
+                //the event source/producer
+                AlarmEventProducer producer = new AlarmEventProducer();
+
+                //event listener/consumer
+                AlarmEventConsumer naren = new AlarmEventConsumer("Narendra");
+                AlarmEventConsumer arben = new AlarmEventConsumer("Arben");
+                AlarmEventConsumer ilia = new AlarmEventConsumer("Ilia");
+
+                //wire up the listener
+                producer.Alarm += new AlarmEventHandler(naren.AlarmRang);
+                producer.Alarm += new AlarmEventHandler(arben.AlarmRang);
+                producer.Alarm += new AlarmEventHandler(ilia.AlarmRang);
+                //uses an anonymous Consumer object
+                producer.Alarm += new AlarmEventHandler(new AlarmEventConsumer("Yin Li").AlarmRang);
+
+                //uses a lambda expression
+                producer.Alarm += new AlarmEventHandler((sender, e) => Console.WriteLine("Lamdba expression was called with " + e.Text));
+
+                //start the clock
+                producer.RunAlarm();
+            }
+        }
+    }
+
+}
